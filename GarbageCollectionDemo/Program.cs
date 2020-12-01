82b8eea1a34bc4f5ca1927b7f9214bcd46b57bcd
“teji1998” <tejasvinirpk@gmail.com>
2020-12-02 00:09:38 +0530

    Added:Welcome message

---
+++ GarbageCollectionDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GarbageCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to world of garbage collection");
+        }
+    }
+}
