d8d96400283ab2d0f0af5826d574da81a24e82f5
“teji1998” <tejasvinirpk@gmail.com>
2020-12-02 00:48:48 +0530

    Added:Code to see total memory used

--- GarbageCollectionDemo/Program.cs
+++ GarbageCollectionDemo/Program.cs
@@ -7,6 +7,26 @@ namespace GarbageCollectionDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to world of garbage collection");
+            long memory1 = GC.GetTotalMemory(false);
+            {
+                //Allocating array and making it unreachable
+                int[] values = new int[50000];
+                values = null;
+            }
+            long memory2 = GC.GetTotalMemory(false);
+            {
+                //collecting garbage
+                GC.Collect();
+
+            }
+            long memory3 = GC.GetTotalMemory(false);
+            {
+                Console.WriteLine(memory1);
+                Console.WriteLine(memory2);
+                Console.WriteLine(memory3);
+            }
+            Console.WriteLine("###############################");
+
         }
     }
 }
